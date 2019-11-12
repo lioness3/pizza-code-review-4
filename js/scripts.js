541b3dba248d40fd396dbd42a1a502fd85c0c642
lioness3 <jcnailtech3@yahoo.com>
2019-11-11 18:00:53 -0800

    create an alert if user does not make a selection

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,19 @@ function Pizza(topping, size, crust){
   this.crust = crust;
   this.price = 0;
 }
-
+//Uses ID in radio input to tell what has been checked
   Pizza.prototype.selectSize = function(){
     if (this.size = medium.checked){
       this.price = 5;
     }else if (this.size = large.checked){
       this.price = 7;
-    }else{
-      this.price = 0;
+    }else {
+      alert("Please, choose either Medium or Large!");
     }
     return this.price;
     };
   Pizza.prototype.selectTopping = function(){
+
     if (this.topping = cheese.checked){
       this.price = 6;
     }else if (this.topping =  pep.checked){
@@ -24,40 +25,41 @@ function Pizza(topping, size, crust){
     }else if (this.topping =  special.checked){
       this.price = 20;
     }else{
-     // warning;
+      this.price = 0;
     }
     return this.price;
     };
   Pizza.prototype.selectCrust = function(){
+
     if (this.crust = thin.checked){
       this.price = 4;
    }else if (this.crust = deep.checked){
      this.price = 8;
    }else if (this.crust = glutenFree.checked){
      this.price = 11;
-   }else{
-     // warning;
-   }
+   }else{}
+
    return this.price;
-};
+  };
 //UI
 $(document).ready(function(){
   $("#order").submit(function(event){
     event.preventDefault();
     var customPizza = new Pizza(toppingSum,sizeSum);
-    var sizeSum =  parseInt(customPizza.selectSize());
+    var sizeSum = customPizza.selectSize();
     var toppingSum = customPizza.selectTopping();
     var crustSum = customPizza.selectCrust();
     var total = (toppingSum + sizeSum + crustSum);
-    console.log(sizeSum);
-    console.log(toppingSum);
-    console.log(crustSum);
+    console.log(total);
+
     $("#displayPrice").text(total);
     $("#order").hide();
+//Clears radio buttons
     $("input[name=size]").prop("checked", false);
     $("input[name=topping]").prop("checked", false);
     $("input[name=crust]").prop("checked", false);
     });
+//Hides form and shows total. Clears total upon "Go Back!"
   $("#goBack").click(function(event){
     event.preventDefault();
     $("#order").toggle();
