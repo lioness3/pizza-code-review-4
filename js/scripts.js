3042d93b6fd3385aecc7409a3fa460caf3587c07
lioness3 <jcnailtech3@yahoo.com>
2019-11-13 21:39:14 -0800

    shorten code

--- js/scripts.js
+++ js/scripts.js
@@ -6,56 +6,29 @@ function Pizza(topping, size, crust){
   this.price = 0;
 }
 //Uses ID in radio input to tell what has been checked
-  Pizza.prototype.selectSize = function(){
-    if (this.size = "medium"){
-      this.price = 5;
-    }else if (this.size = "large"){
-      this.price = 7;
-    }else {
-      alert("Please, choose either Medium or Large!")
-// Clears totaled price if size is not selected.
-      document.getElementById("displayPrice").reset();
-    }
-    return this.price;
-    };
-  Pizza.prototype.selectTopping = function(){
-    if (this.topping = "cheese"{
-      this.price = 6;
-    }else if (this.topping =  "pep"){
-      this.price = 8;
-    }else if (this.topping =  "special"){
-      this.price = 20;
-    }else{
-//Does not add to the price if no selection is made.
-      this.price = 0;
-    }
-    return this.price;
-    };
-  Pizza.prototype.selectCrust = function(){
-    if (this.crust = "thin"){
-      this.price = 4;
-   }else if (this.crust = "deep"){
-     this.price = 8;
-   }else if (this.crust = "glutenFree"){
-     this.price = 11;
-   }else{
-     alert("Please select a crust!")
-// Clears totaled price if crust is not selected.
-   }
-   return this.price;
-  };
+  Pizza.prototype.cost = function(){
+  this.topping == "checked";
+  this.size == "checked";
+  this.crust == "checked";
+
+  return this.price;
+
+};
+//      alert("Please select a crust!")
+//
+// // Clears totaled price if crust is not selected.
+// // Clears totaled price if size is not selected.
+// document.getElementById("displayPrice").reset();
 //UI
 $(document).ready(function(){
   $("#order").submit(function(event){
     event.preventDefault();
-    var customPizza = new Pizza(toppingSum,sizeSum);
-    var sizeSum = customPizza.selectSize();
-    var toppingSum = customPizza.selectTopping();
-    var crustSum = customPizza.selectCrust();
-    var total = (toppingSum + sizeSum + crustSum);
-    console.log(total);
-
-    $("#displayPrice").text(total);
+    var topping = $("input[name='topping']:checked").val();
+    var size = $("input[name='size']:checked").val();
+    var crust = $("input[name='crust']:checked").val();
+    var customPizza = new Pizza(topping, size, crust);
+    customPizza.cost;
+    $("#displayPrice").text(customPizza.price);
     $("#order").hide();
 //Clears radio buttons
     $("input[name=size]").prop("checked", false);
